4355b212e7952978c5318b599672426826f37205
moisesmarq <moises.marques@outlook.com>
2025-01-08 23:19:08 -0300

    Usando repository e tabelas sendo criadas dinamicamente

--- src/main/java/br/com/moisesmarques/todolist/user/UserController.java
+++ src/main/java/br/com/moisesmarques/todolist/user/UserController.java
@@ -1,18 +1,23 @@
 package br.com.moisesmarques.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.var;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
-        System.out.println(userModel.getUsername());
-        System.out.println(userModel.getPassword());
+    public UserModel create(@RequestBody UserModel userModel) {
+      var userCreated =  this.userRepository.save(userModel);
+      return userCreated;
     }
 }
