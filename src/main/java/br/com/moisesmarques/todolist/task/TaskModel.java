94851f6e84e3914a217926a41041ffa5e8b2b555
moisesmarq <moises.marques@outlook.com>
2025-02-13 21:44:24 -0300

    Criando a entidade de Tasks

---
+++ src/main/java/br/com/moisesmarques/todolist/task/TaskModel.java
@@ -0,0 +1,46 @@
+package br.com.moisesmarques.todolist.task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+     * 
+     * Usuário (id_Usuario)
+     * Descrição
+     * Titulo
+     * Data de Inicio
+     * Data de Termino
+     * Prioridade
+     * 
+     */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
