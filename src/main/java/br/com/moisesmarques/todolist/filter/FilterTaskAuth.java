61979e7e985b3c1f9a1ff36acb705cd83128c035
moisesmarq <moises.marques@outlook.com>
2025-02-13 23:47:31 -0300

    Criando filtro de autenticao - validacao das credencias

--- src/main/java/br/com/moisesmarques/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/moisesmarques/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.moisesmarques.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.moisesmarques.todolist.user.IUserRepository;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -17,6 +20,10 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    //Por padrão esse filtro está sendo aplicado para todas as rotas
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -44,8 +51,27 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String username = credentials[0];
         String password = credentials[1];
         
-        System.out.println(username);
-        System.out.println(password);
-        filterChain.doFilter(request, response);
+        //Validacao do usuario
+        var user = this.userRepository.findByUsername(username);
+        if (user == null)
+        {
+           response.sendError(401);
+        }
+        else
+        {
+            //Validacao da senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+           
+            if(passwordVerify.verified)
+            {
+                filterChain.doFilter(request, response);
+            }
+            else
+            {
+                response.sendError(401);
+            }
+            
+            
+        }
     }
 }
